3954bb3a3cb1f64176fe00e32ab743480cdf5b7f
Adrian <adrianvalverde45@gmail.com>
2023-06-28 12:22:45 -0400

    commit tree f6411343b18c1da955187d62f43f2721831ef4f8

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,90 @@
 # your code goes here
+# I am a sentence. are you?
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(current_hygiene)
+        if current_hygiene >= 10
+            @happiness = 10
+        elsif current_hygiene < 0
+            @happiness = 0
+        else
+            @happiness = current_hygiene
+        end
+
+    end
+
+    def hygiene=(current_hygiene)
+        if current_hygiene >= 10
+            @hygiene = 10
+        elsif current_hygiene < 0
+            @hygiene = 0
+        else
+            @hygiene = current_hygiene
+        end
+    end
+
+    def happy?
+        if self.happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def clean?
+        if self.hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid(amount)
+        @bank_account += amount
+
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness +=3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+        
+    end
+
+end
